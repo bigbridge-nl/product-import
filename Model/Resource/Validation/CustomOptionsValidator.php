fffc66281d9583921954521f7c7375f1e8acf062
Patrick <patrick@bigbridge.nl>
2018-08-21 17:08:21 +0200

    Check for number of custom options values is different for store views

--- Model/Resource/Validation/CustomOptionsValidator.php
+++ Model/Resource/Validation/CustomOptionsValidator.php
@@ -3,6 +3,7 @@
 namespace BigBridge\ProductImport\Model\Resource\Validation;
 
 use BigBridge\ProductImport\Api\Data\Product;
+use BigBridge\ProductImport\Api\Data\ProductStoreView;
 
 /**
  * @author Patrick van Bergen
@@ -22,9 +23,21 @@ class CustomOptionsValidator
         foreach ($customOptions as $customOption) {
             $values = $customOption->getValues();
             if ($values) {
+                $count = count($values);
                 foreach ($product->getStoreViews() as $storeViewCode => $storeView) {
                     $storeViewValues = $storeView->getCustomOptionValues($customOption);
-                    if (count($storeViewValues) != count($values)) {
+                    $storeViewCount = count($storeViewValues);
+                    $error = false;
+                    if ($storeViewCode === Product::GLOBAL_STORE_VIEW_CODE) {
+                        if ($storeViewCount != $count) {
+                            $error = true;
+                        }
+                    } else {
+                        if (($storeViewCount > 0) && ($storeViewCount != $count)) {
+                            $error = true;
+                        }
+                    }
+                    if ($error) {
                         $product->addError("Custom option with values [" . implode(', ', $values) . "] has an incorrect number of values in store view '{$storeViewCode}'");
                     }
                 }
