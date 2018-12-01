a9aa3c2eca453c6e0647c30cbd2562f67702be49
Patrick van Bergen <patrick.vanbergen@gmail.com>
2018-12-01 09:52:36 +0100

    Make integration test import all example xml files.

--- Test/Integration/ProductReaderTest.php
+++ Test/Integration/ProductReaderTest.php
@@ -3,8 +3,13 @@
 namespace BigBridge\ProductImport\Test\Integration;
 
 use BigBridge\ProductImport\Api\ImportConfig;
+use BigBridge\ProductImport\Model\Data\EavAttributeInfo;
+use BigBridge\ProductImport\Model\Persistence\Magento2DbConnection;
 use BigBridge\ProductImport\Model\Reader\ProductImportWebApiLogger;
 use BigBridge\ProductImport\Model\Reader\XmlProductReader;
+use BigBridge\ProductImport\Model\Resource\MetaData;
+use Exception;
+use Magento\Framework\App\ObjectManager;
 
 /**
  * @author Patrick van Bergen
@@ -15,18 +20,64 @@ class ProductReaderTest extends \Magento\TestFramework\TestCase\AbstractControll
     {
         $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
+        $this->setupAttributes($objectManager);
+
         /** @var XmlProductReader $productReader */
         $productReader = $objectManager->create(XmlProductReader::class);
 
-        $path = __DIR__ . "/resources/test-products.xml";
+        foreach (glob(__DIR__ . '/../../doc/example/*.xml') as $xmlFile) {
 
-        $logger = new ProductImportWebApiLogger();
+            $success = true;
 
-        $config = new ImportConfig();
-        $config->resultCallback = [$logger, 'productImported'];
+            $logger = new ProductImportWebApiLogger();
 
-        $productReader->import($path, $config, false, $logger);
+            try {
 
-        $this->assertSame(1, $logger->getOkProductCount());
+                $config = new ImportConfig();
+                $config->imageSourceDir = __DIR__ . '/../../doc/example';
+                $config->autoCreateOptionAttributes = ['color', 'manufacturer', 'color_group'];
+                $config->resultCallback = [$logger, 'productImported'];
+
+                $productReader->import($xmlFile, $config, false, $logger);
+
+                if ($logger->getFailedProductCount() > 0) {
+                    $success = false;
+                }
+
+            } catch (Exception $exception) {
+                $success = false;
+            }
+
+            if (!$success) {
+                echo "\n";
+                echo "Error in " . $xmlFile . ":\n";
+                echo $logger->getOutput() . "\n";
+            }
+
+            $this->assertSame(0, $logger->getFailedProductCount());
+            $this->assertGreaterThan(0, $logger->getOkProductCount());
+        }
+    }
+
+    private function setupAttributes(ObjectManager $objectManager)
+    {
+        $metaData = $objectManager->get(MetaData::class);
+        $db = $objectManager->get(Magento2DbConnection::class);
+
+        // create a multiple select attribute
+        $db->execute("
+            REPLACE INTO " . $metaData->attributeTable . "
+            SET 
+                entity_type_id = " . $metaData->productEntityTypeId . ",
+                attribute_code = 'color_group_product_importer',
+                frontend_input = 'multiselect',
+                backend_type = 'varchar'
+        ");
+
+        $insertId = $db->getLastInsertId();
+
+        $metaData->productEavAttributeInfo['color_group'] =
+            new EavAttributeInfo('color_group', $insertId, false, 'varchar', 'catalog_product_entity_varchar', 'multiselect', 1);
+
     }
 }
